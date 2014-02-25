d5ddec8bc1a5daba1f146714bba90f4c595fa9e4
soffolk <zlx.star@gmail.com>
2014-02-25 19:01:56 +0800

    Fetch remain count for smsbao

--- lib/china_sms/service/smsbao.rb
+++ lib/china_sms/service/smsbao.rb
@@ -5,6 +5,7 @@ module ChinaSMS
       extend self
 
       URL = "http://api.smsbao.com/sms"
+      REMAIN_URL = "http://www.smsbao.com/query"
 
       MESSAGES = {
         '0'  => '短信发送成功',
@@ -21,6 +22,18 @@ module ChinaSMS
         phones = Array(phone).join(',')
         res = Net::HTTP.post_form(URI.parse(URL), u: options[:username], p: Digest::MD5.hexdigest(options[:password]), m: phones, c: content)
         result res.body
+      end
+
+      def get options
+        res = Net::HTTP.post_form(URI.parse(REMAIN_URL), u: options[:username], p: Digest::MD5.hexdigest(options[:password]))
+        r = res.body.match(/(\d+)\n(\d+),(\d+)/)
+        {
+          success: (r[1] == '0'),
+          code: r[1],
+          message: MESSAGES[r[1]],
+          send: r[2].to_i,
+          remain: r[3].to_i
+        }
       end
 
       def result(code)
