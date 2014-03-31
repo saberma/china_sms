143e1925bd86be59e00aad2047d27bc6ca22f010
lyfeyaj <lyfeyaj@gmail.com>
2014-03-31 23:25:20 +0800

    add tests for yunpian

--- spec/service/yunpian_spec.rb
+++ spec/service/yunpian_spec.rb
@@ -13,13 +13,31 @@ describe "Yunpian" do
 
       before do
         stub_request(:post, url).
-          with(body: {apikey: apikey, mobile: phone, tpl_id: 2, tpl_value: "#code#=云片测试：验证码 1234。&#company#=云片网" },
-               headers: {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/x-www-form-urlencoded', 'Host'=>'yunpian.com', 'User-Agent'=>'Ruby'}).
-          to_return(body: {"code" => 0, "msg" => "OK", "result" => {"count" => 1, "fee" => 1, "sid" => 592762800}}.to_json )
+          with(
+            :body => {
+              "apikey" => apikey,
+              "mobile" => phone,
+              "tpl_id" => "2",
+              "tpl_value" =>"#code#=#{content}&#company#=云片网"},
+            :headers => {
+              'Content-Type' => 'application/x-www-form-urlencoded'
+            }
+          ).
+          to_return(
+            body: {
+              'code' => 0,
+              'msg' => 'OK',
+              'result' => {
+                'count' => '1',
+                'fee' => '1',
+                'sid' => '592762800'
+              }
+            }.to_json
+          )
       end
 
-      its([:code]) { should eql 0 }
-      its([:msg]) { should eql "OK" }
+      its(["code"]) { should eql 0 }
+      its(["msg"]) { should eql "OK" }
     end
 
     context 'invalid key' do
@@ -28,13 +46,22 @@ describe "Yunpian" do
 
       before do
         stub_request(:post, url).
-          with(body: {}).
+          with(
+            :body => {
+              "apikey" => apikey,
+              "mobile" => phone,
+              "tpl_id" => "2",
+              "tpl_value" =>"#code#=#{content}&#company#=云片网"},
+            :headers => {
+              'Content-Type' => 'application/x-www-form-urlencoded'
+            }
+          ).
           to_return(body: {"code" => -1, "msg" => "非法的apikey", "detail" => "请检查的apikey是否正确"}.to_json )
       end
 
-      its([:code]) { should eql -1 }
-      its([:msg]) { should eql "非法的apikey" }
-      its([:detail]) { should eql "请检查的apikey是否正确" }
+      its(["code"]) { should eql -1 }
+      its(["msg"]) { should eql "非法的apikey" }
+      its(["detail"]) { should eql "请检查的apikey是否正确" }
     end
 
   end
