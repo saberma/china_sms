8fed961a7933fb1e9a737a542964112e4c904aef
lyfeyaj <lyfeyaj@gmail.com>
2014-03-31 23:56:33 +0800

    add-readme

--- spec/service/yunpian_spec.rb
+++ spec/service/yunpian_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 
 describe "Yunpian" do
   describe "#to" do
-    let(:apikey) { '2022b1529967a8cb788c05ddd9fc339e' }
+    let(:apikey) { '2022b1599967a8cb788c05ddd9fc339e' }
     let(:url) { "http://yunpian.com/v1/sms/tpl_send.json" }
     let(:content) { '云片测试：验证码 1234。' }
     subject { ChinaSMS::Service::Yunpian.to phone, content, password: apikey }
@@ -48,15 +48,21 @@ describe "Yunpian" do
         stub_request(:post, url).
           with(
             :body => {
-              "apikey" => apikey,
-              "mobile" => phone,
-              "tpl_id" => "2",
+              "apikey"    => apikey,
+              "mobile"    => phone,
+              "tpl_id"    => "2",
               "tpl_value" =>"#code#=#{content}&#company#=云片网"},
             :headers => {
               'Content-Type' => 'application/x-www-form-urlencoded'
             }
           ).
-          to_return(body: {"code" => -1, "msg" => "非法的apikey", "detail" => "请检查的apikey是否正确"}.to_json )
+          to_return(
+            body: {
+              "code"   => -1,
+              "msg"    => "非法的apikey",
+              "detail" => "请检查的apikey是否正确"
+            }.to_json
+          )
       end
 
       its(["code"]) { should eql -1 }
